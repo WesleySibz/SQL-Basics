fc090cb288ff3d8282d616e1d40aae1f14fedaa8
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-06-22 10:38:51 +0200

    Type of Triangle

--- Type of Triangle.sql
+++ Type of Triangle.sql
@@ -4,11 +4,14 @@
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
-Select 
+SELECT 
 CASE
-when A + B <= C or A + C <= B or B + C <= A then "Not A Triangle"
-when A = B and B = C then "Equilateral"
-when A = B or A = C or B = C then "Isosceles"
-else "Scalene"
-end as triangle_sides
-from TRIANGLES
+WHEN (A+B>C) AND (A+C>B) AND (B+C>A) THEN
+CASE
+WHEN (A=B) AND (B=C) THEN 'Equilateral'
+WHEN (A=B) OR (B=C) OR (A=C) THEN 'Isosceles'
+ELSE 'Scalene'
+END 
+ELSE 'Not a triangle'
+END
+FROM Triangles;
